e0cbffda5e91131167feec30c5ed6753fa3712e2
TaeHwan <taehwan@thdev.net>
2016-08-30 22:01:07 +0900

    Initial commit

---
+++ samplebase/src/main/java/tech/thdev/samplebase/Sample.java
@@ -0,0 +1,14 @@
+package tech.thdev.samplebase;
+
+import android.util.Log;
+
+/**
+ * Created by tae-hwan on 8/30/16.
+ */
+
+public class Sample {
+
+    public static void print() {
+        Log.i("TAG", "Sample print");
+    }
+}
