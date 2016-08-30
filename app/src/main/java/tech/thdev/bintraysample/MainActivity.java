f1e3570f2582f7b3a8b876d1354b7e236fcb67ce
TaeHwan <taehwan@thdev.net>
2016-08-31 00:56:52 +0900

    Update UploadBintray!! and create install.
    create sample code

--- app/src/main/java/tech/thdev/bintraysample/MainActivity.java
+++ app/src/main/java/tech/thdev/bintraysample/MainActivity.java
@@ -3,11 +3,15 @@ package tech.thdev.bintraysample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import tech.thdev.samplebase.Sample;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Sample.print();
     }
 }
